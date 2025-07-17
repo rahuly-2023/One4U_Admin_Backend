b52fff7fae469eb4caf68c286bcc27b3e479a29a
“Rahul <rahul2483yadav@gmail.com>
2025-07-17 17:16:07 +0530

    feat: added admin Model for admin based authentication

---
+++ Models/Admin.js
@@ -0,0 +1,11 @@
+// admin_backend/Models/User.js
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Admin', userSchema);
