81437afe387e08be764e835f9de0b9494dd0c508
“Rahul <rahul2483yadav@gmail.com>
2025-07-17 16:03:05 +0530

    Update: Dpdated JWT secret

--- routes.js
+++ routes.js
@@ -78,7 +78,7 @@ const authenticateAdmin = (req, res, next) => {
   
   if (!token) return res.status(401).json({ message: 'Unauthorized' });
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'chirag');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     // if (decoded.role !== 'admin') {
     //   return res.status(403).json({ message: 'Forbidden' });
     // }
